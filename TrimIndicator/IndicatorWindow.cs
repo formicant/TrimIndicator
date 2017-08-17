da8216bb5f95bb47472ac1805ebeef8cc2c2eb89
formicant <formicant@gmail.com>
2017-08-17 23:53:01 +0700

    Version 1.3.0.0 - Mod functionality added

---
+++ TrimIndicator/IndicatorWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TrimIndicator
+{
+	[KSPAddon(KSPAddon.Startup.Flight, once: false)]
+	public class TrimIndicatorAddon : MonoBehaviour
+	{
+		void Start()
+		{
+			var gaugesObject = FindObjectOfType<KSP.UI.Screens.Flight.LinearControlGauges>().gameObject;
+
+			_trimPitch = new TrimLabel(gaugesObject, new Vector3(34F, -15.5F), true);
+			_trimYaw = new TrimLabel(gaugesObject, new Vector3(-24F, -63.5F), false);
+			_trimRoll = new TrimLabel(gaugesObject, new Vector3(-24F, -25F), false);
+			//_trimWheelThrottle = new TrimLabel(gaugesObject, new Vector3(), true);
+			//_trimWheelSteer = new TrimLabel(gaugesObject, new Vector3(), false);
+		}
+
+		void Update()
+		{
+			var ctrlState = FlightGlobals.ActiveVessel?.ctrlState;
+
+			_trimPitch.SetValue(ctrlState?.pitchTrim ?? 0);
+			_trimYaw.SetValue(ctrlState?.yawTrim ?? 0);
+			_trimRoll.SetValue(ctrlState?.rollTrim ?? 0);
+			//_trimWheelThrottle.SetValue(ctrlState?.wheelThrottleTrim ?? 0);
+			//_trimWheelSteer.SetValue(ctrlState?.wheelSteerTrim ?? 0);
+		}
+
+		TrimLabel _trimPitch;
+		TrimLabel _trimYaw;
+		TrimLabel _trimRoll;
+		//TrimLabel _trimWheelThrottle;
+		//TrimLabel _trimWheelSteer;
+	}
+}
---
+++ TrimIndicator/TrimLabel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace TrimIndicator
+{
+	class TrimLabel
+	{
+		public TrimLabel(GameObject parent, Vector3 location, bool isVertical)
+		{
+			_sliderDirection = isVertical ? Vector3.up : Vector3.right;
+			_sliderAmplitude = isVertical ? VerticalSliderAmplitude : HorizontalSliderAmplitude;
+			var textAlignment = isVertical ? TextAlignmentOptions.BaselineLeft : TextAlignmentOptions.Baseline;
+
+			var baseObject = MakeDummyObject(parent, location);
+			_slider = MakeDummyObject(baseObject);
+
+			_textMeshes = new[]
+			{
+				MakeTextMesh(_slider, -OutlineWidth * _sliderDirection, BackgroundColor, textAlignment),
+				MakeTextMesh(_slider, +OutlineWidth * _sliderDirection, BackgroundColor, textAlignment),
+				MakeTextMesh(_slider, Vector3.zero, ForegroundColor, textAlignment),
+			};
+		}
+
+		public void SetValue(float value)
+		{
+			int steps = (int)Math.Round(500 * value);
+			string text = steps != 0
+				? (steps < 0 ? "−" : "+") + Math.Abs(steps).ToString(CultureInfo.InvariantCulture)
+				: string.Empty;
+
+			_slider.transform.localPosition = _sliderAmplitude * GetSliderPosition(value) * _sliderDirection;
+
+			foreach(var textMesh in _textMeshes)
+				textMesh.text = text;
+		}
+
+		readonly Vector3 _sliderDirection;
+		readonly float _sliderAmplitude;
+		readonly GameObject _slider;
+		readonly IEnumerable<TextMeshProUGUI> _textMeshes;
+
+
+		static GameObject MakeObject(GameObject parent, Vector3 relativeLocation)
+		{
+			var gameObject = new GameObject { layer = LayerMask.NameToLayer("UI") };
+			gameObject.transform.SetParent(parent.transform, false);
+			gameObject.transform.localPosition = relativeLocation;
+			return gameObject;
+		}
+
+		static GameObject MakeDummyObject(GameObject parent, Vector3 relativeLocation = default(Vector3))
+		{
+			var gameObject = MakeObject(parent, relativeLocation);
+			// Just an empty text mesh. Consider replacing by something more adequate
+			gameObject.AddComponent<TextMeshProUGUI>();
+			return gameObject;
+		}
+
+		static TextMeshProUGUI MakeTextMesh(GameObject parent, Vector3 relativeLocation, Color color, TextAlignmentOptions alignment)
+		{
+			var gameObject = MakeObject(parent, relativeLocation);
+
+			var textMesh = gameObject.AddComponent<TextMeshProUGUI>();
+			textMesh.autoSizeTextContainer = true;
+			textMesh.isOverlay = true;
+			textMesh.enableWordWrapping = false;
+			textMesh.alignment = alignment;
+			textMesh.font = Font;
+			textMesh.fontMaterial = FontMaterial;
+			textMesh.fontSize = FontSize;
+			textMesh.color = color;
+
+			return textMesh;
+		}
+
+		static float GetSliderPosition(float value) =>
+			(float)(Math.Atan(value * SliderPositionSteepness) / Math.Atan(SliderPositionSteepness));
+
+		static TMP_FontAsset _font;
+		static TMP_FontAsset Font => _font ?? (_font =
+			Resources.LoadAll<TMP_FontAsset>("Fonts")
+				.FirstOrDefault(f => f.name == "Calibri SDF"));
+
+		static Material _fontMaterial;
+		static Material FontMaterial => _fontMaterial ?? (_fontMaterial =
+			Resources.LoadAll<Material>("Fonts")
+				.FirstOrDefault(f => f.name == "Calibri SDF Material"));
+
+		static readonly Color BackgroundColor = new Color(0.235F, 0.274F, 0.310F); // #3C464F
+		static readonly Color ForegroundColor = new Color(0.827F, 0.827F, 0.827F); // #D3D3D3
+		const float FontSize = 17F;
+		const float OutlineWidth = 1.5F;
+		const float HorizontalSliderAmplitude = 39F;
+		const float VerticalSliderAmplitude = 49F;
+		const double SliderPositionSteepness = 20;
+	}
+}
